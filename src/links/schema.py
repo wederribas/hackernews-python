88eaaaa49d4e3c64516e471afc20f44b7dd288c7
wederribas <wederribas@gmail.com>
2018-07-17 08:05:23 -0300

    Add relationship between a user and a posted link. Also migrate it.

--- src/links/schema.py
+++ src/links/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from .models import Link
+from users.schema import UserType
 
 
 class LinkType(DjangoObjectType):
@@ -20,19 +21,27 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     class Arguments:
         url = graphene.String()
         description = graphene.String()
 
     def mutate(self, info, url, description):
-        link = Link(url=url, description=description)
+        user = info.context.user or None
+
+        link = Link(
+            url=url,
+            description=description,
+            posted_by=user,
+        )
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
             description=link.url,
+            posted_by=link.posted_by,
         )
 
 
